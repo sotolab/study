aa126d74fbfcc7800dde6c6f8f9836ac4c5c02f6
sotojang <mrjang28@gmail.com>
2019-12-23 22:01:20 +0900

    Update simToken.sol

--- simToken.sol
+++ simToken.sol
@@ -8,12 +8,17 @@ contract MyToken {
    // Initializes contract with initial supply tokens to the creator of the 
    //contract
 
+
+    // 1 deploy: 200000
    constructor (uint256 initialSupply) public payable {
        // Give the creator all initial tokens 
        balanceOf[msg.sender] = initialSupply;   
    }
 
    // Send coins
+   // 2 balanceOf: "0xca35b7d915458ef540ade6068dfe2f44e8fa733c" --> 200000
+   // 3 transfer: "0x14723a09acff6d2a60dcdf7aa4aff308fddc160c", 10
+   // 4 balanceOf: "0x14723a09acff6d2a60dcdf7aa4aff308fddc160c" --> 10
    function transfer(address _to, uint256 _value) public payable {
 
     // Check if the sender has enough
